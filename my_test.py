80a475a37e0c80bdf9a65094dac80d747ab43d55
21lsparks <21lsparks@athenian.org>
2020-10-08 17:35:47 +0000

    ideas
    Fixes Athenian-ComputerScience-Fall2020/fix-code-1-21lsparks#2

--- my_test.py
+++ my_test.py
@@ -1,7 +1,25 @@
-from my_code import times_3
+# i should use that f string thing with the fancy curlique brackets to show the user what they input, 
+# so if they get weird results, they can check to see if they accidentally input the wrong things
+# can i make it like a checklist, so I assign variables to different options for different questions
+#(there should only be 2-3 options for each question so this is feasible) and I could say, if its dry, type D, if its 
+# sandy, type Y, and could assign those letters, and could say if (need variable for plants) == Y, then run, or something
+# like that, and can use if/elif/else statements to check if it matches, can I make it like a funnel?
+# would it work like a funnel if I had multiple while loops running inside of each other? or some kind of loop thing
+# where there are loops in loops, and it would probably need to start in the smallest loop, and then go to the next and the next,
+# until at the end, I get to the loop that is the least, and the plan would be that at the beginning the program accepted all of the plants, 
+# and then it cut out the ones that didnt match the first criteria, and then there are some that didnt match the later criteria
+# and it keeps cutting out the ones that dont have the same answer in each loop, and then what would ulimately happen, is that 
+# if there were no plants that matched all of the criteria, it would say "sorry there are no plants that match those criteria. Try again and 
+# ajust the criteria" which would be maybe annoying to the user, but that can happen on zillow too, and its annoying but we 
+# make do. And I don't think I can make it so the user can adjust the criteria to which are most important, 
+# but if I could figure it out, that would be the best, but that would I think involve giving the user the ability
+# to change the code, because of the loop system, if it works, that i came up with
+# it would be cool, if, at the end, I could ask for their zipcode, and then spit out all of the places they 
+# might be able to buy the seeds/seedlings that they wanted, but then again, maybe that is unhelpful, because I cant tell them
+# if the place has them in stock, maybe that could be another project though? that could be cool
+# (1)sun v. part sun v. shade, (3)basic v. acidic, v. nuetrual v. opt out, (2)sand v. clay
+# (5)high water v. low water, v. medium water, (4)plant in summer v. winter v. spring v. fall, (6) annual v. perrenial --> lasts a year or lasts 5 years?
+# 
 
-
-def test_times_3():
-    assert 15 == times_3("5")
-    assert 0 == times_3("0")
-    assert -3 == times_3("-1")
+# radishes, sandy (needs airaited soil, so doesnt do well in clay), winter, medium water (moist but no wet), sun/ part sun, acidic
+# lettuce, fall planting, medium water, 
